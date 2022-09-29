8b9eaadb4480fde4777406da2ad4f107d0aa881c
David Vanderburgh <david.vanderburgh@cardinalhealth.com>
2022-09-29 14:31:02 -0400

    feat: proof of concept image scraping

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,32 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import puppeteer, { Browser, HTTPResponse, Page } from 'puppeteer';
+import path from 'path';
+import fs, { WriteStream } from 'fs';
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+const handler = async (
   req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  res: NextApiResponse<string[]>
+): Promise<void> => {
+  const browser: Browser = await puppeteer.launch();
+  const page: Page = await browser.newPage();
+  const fileNames: string[] = [];
+
+  page
+    .on('response', async (response: HTTPResponse) => {
+      const url: string = response.url();
+      if (response.request().resourceType() === 'image') {
+        response.buffer().then((file: Buffer) => {
+          const fileName: string = url.split('/').pop() ?? '';
+          const filePath: string = `./public/images/${fileName}`;
+          const writeStream: WriteStream = fs.createWriteStream(filePath);
+          writeStream.write(file);
+          fileNames.push(fileName);
+        });
+      }
+    });
+  await page.goto('https://www.cabinsbythecaves.com/Rosewood-Lodge-Hocking-Hills.html');
+  await browser.close();
+  res.status(200).json(fileNames);
 }
+
+export default handler;
